e09034b0c8d8915dbe4ecf78d44cc309e6e21ce5
victordoyle <victor-doyle@outlook.com>
2021-04-28 11:21:13 -0400

    profile user css changes

--- stream-helper/src/pages/Profile.js
+++ stream-helper/src/pages/Profile.js
@@ -10,12 +10,19 @@ import {
   Dropdown,
   ButtonGroup,
   Image,
+  Card
 } from "react-bootstrap";
 import "../styles/Profile.css";
 import SavedMoviesModal from "../components/Modals/SavedMoviesModal";
 import EditUserModal from "../components/Modals/EditUserModal";
 import WatchedMoviesModal from "../components/Modals/WatchedMoviesModal";
 import DislikedMoviesModal from "../components/Modals/DislikedMoviesModal";
+import imgLikedMovies from "../media/imgLikedMovies.png"
+import imgSavedMovies from "../media/imgSavedMovies.png"
+import imgEditProfile from "../media/imgEditProfile.png"
+
+
+
 /* GraphQl */
 import { useMutation } from "@apollo/client";
 import { UPDATEUSERPROFILE } from "../graphql/operations";
@@ -66,34 +73,42 @@ function Profile({ history }) {
 
             <div className="profileContentContainer">
               <div className="profileContentItems">
-                {/* user Profile Image */}
-                <Image src="" rounded />
-                <h1>
-                  {" "}
-                  User Bio here: A bunch of random string text and Charfield{" "}
-                  <br />
-                  content to fill up this space
-                </h1>
+                
 
                 {/* Modal For Liked Movies */}
-                <DropdownButton
-                  as={ButtonGroup}
-                  title="Watched/Disliked Movies"
-                  id="bg-nested-dropdown"
-                >
-                  <Dropdown.Item eventKey="1">
-                    <WatchedMoviesModal />
-                  </Dropdown.Item>
-                  <Dropdown.Item eventKey="2">
-                    <DislikedMoviesModal />
-                  </Dropdown.Item>
-                </DropdownButton>
+                <Card style={{ width: '18rem' }} className="likedMoviesModal">
+                  <Card.Img variant="top" src={imgLikedMovies} />
+                  <Card.Body>
+                      <DropdownButton
+                        as={ButtonGroup}
+                        title="Show Me!"
+                        id="bg-nested-dropdown"
+                        style={{paddingTop: '25px'}}
+                      >
+                        <Dropdown.Item eventKey="1">
+                          <WatchedMoviesModal />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="2">
+                          <DislikedMoviesModal />
+                        </Dropdown.Item>
+                      </DropdownButton>
+                  </Card.Body>
+                </Card>
 
                 {/* Modal For Saved Movies*/}
-                <SavedMoviesModal />
+
+                <Card style={{ width: '18rem' }} className="savedMoviesModal">
+                  <Card.Img variant="top" src={imgSavedMovies} />
+                  <Card.Body>     
+                  <SavedMoviesModal />
+                  </Card.Body>
+                </Card>
+                
 
                 {/* Modal For Profile Edit */}
-
+                <Card style={{ width: '18rem' }} className="editUserModal">
+                  <Card.Img variant="top" src={imgEditProfile} />
+                  <Card.Body>  
                 <EditUserModal
                   firstname={(e) => setFirstName(e.target.value)}
                   lastname={(e) => setLastName(e.target.value)}
@@ -101,6 +116,8 @@ function Profile({ history }) {
                   username={(e) => setUserName(e.target.value)}
                   submit={submitProfileEdit}
                 />
+                  </Card.Body>
+                </Card>
               </div>
             </div>
           </>
